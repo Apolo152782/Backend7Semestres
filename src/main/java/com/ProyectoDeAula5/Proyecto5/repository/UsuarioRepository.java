b6b156d505b9d454704e553e967f58819ad82aca
Brayan Marrugo <148504395+Apolo152782@users.noreply.github.com>
2026-05-08 15:33:12 -0500

    Remove duplicate findByCorreo method

--- src/main/java/com/ProyectoDeAula5/Proyecto5/repository/UsuarioRepository.java
+++ src/main/java/com/ProyectoDeAula5/Proyecto5/repository/UsuarioRepository.java
@@ -7,7 +7,6 @@ import com.ProyectoDeAula5.Proyecto5.model.Usuario;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<Usuario> findByCorreo(String correo);
 
     Optional<Usuario> findByCorreoAndPass(String correo, String pass);
 
