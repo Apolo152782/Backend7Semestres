d46341a78966eb1e1c20401842745314a9af4d54
Brayan Marrugo <148504395+Apolo152782@users.noreply.github.com>
2026-05-08 15:37:25 -0500

    Add PutMapping and CrossOrigin imports to UsuarioController

--- src/main/java/com/ProyectoDeAula5/Proyecto5/controller/UsuarioController.java
+++ src/main/java/com/ProyectoDeAula5/Proyecto5/controller/UsuarioController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
