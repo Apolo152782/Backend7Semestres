b76760478ed083611d6a285355a602111c1d5449
Brayan Marrugo <148504395+Apolo152782@users.noreply.github.com>
2026-05-11 17:36:10 -0500

    Create IAResponseDTO class for response data

---
+++ src/main/java/com/ProyectoDeAula5/Proyecto5/service/dto/IAResponseDTO.java
@@ -0,0 +1,13 @@
+package com.ProyectoDeAula5.Proyecto5.service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IAResponseDTO {
+
+    private String resultado;
+
+    private Double probabilidad;
+}
